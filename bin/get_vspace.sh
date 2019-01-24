136282054136d6229250fb10bf750ab3a1376fa9
Caitlyn Wilhelm <cwilhelm@uw.edu>
2019-01-23 22:32:26 -0800

    Added line to go to new save location

--- bin/get_vspace.sh
+++ bin/get_vspace.sh
@@ -13,39 +13,39 @@ fi
 
 #---------------------------------------------------
 # Check to see if file exists
-
+cd ~/Projects/IceBelt/vspace_files/G_Star
 if [[ -e $VSPACE_FILE ]]
 then
 	echo "VSPACE_FILE : $VSPACE_FILE"
 	#ls -l $VSPACE_FILE
     SEMI=$(grep ^dSemi $VSPACE_FILE)
     LUM=$(grep ^dLuminosity $VSPACE_FILE)
-    
+
     if [[ $SEMI == *'['*']'* ]]
     then
         YAXIS="A"
         YMIN=$(echo $SEMI | tr -d '[,]' | awk '{print $2}')
         YMAX=$(echo $SEMI | tr -d '[,]' | awk '{print $3}')
     fi
-    
+
     if [[ $LUM == *'['*']'* ]]
     then
         YAXIS="S"
     fi
-    
+
     #---------------------------------------------------
     # assigns the destination folder to a variable
-    
+
     DESTFOLDER=$(grep ^destfolder $VSPACE_FILE | awk '{print $2}')
-    
+
     #---------------------------------------------------
 	# assigns the grid witdth/length to a variable
-	
+
 	NUM=$(grep ^dObliquity $VSPACE_FILE | tr -d '[]' | cut -f2 -d 'n' | awk '{print $1}')
-    
+
     #---------------------------------------------------
 	# assigns name of subfolders to a variable
-	
+
 	SUB=$(grep ^trialname $VSPACE_FILE | awk '{print $2}')
 
 fi
@@ -56,7 +56,6 @@ fi
 echo "DESTFOLDER : $DESTFOLDER"
 echo "NUM : $NUM"
 echo "SUB : $SUB"
-echo "YAXIS : $YAXIS" 
+echo "YAXIS : $YAXIS"
 
 #---------------------------------------------------
-
